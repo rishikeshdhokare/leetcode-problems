ac6d59450a7a45477ba08d1675816f4286daef07
Ritesh Puj <ritesh.puj@zalando.de>
2020-10-26 20:29:40 +0100

    implementStrStr naive approach and Knutt morris algo

---
+++ src/main/kotlin/array/ImplementStrStr.kt
@@ -0,0 +1,74 @@
+package array
+
+@Suppress("unused")
+class ImplementStrStr {
+    /*
+    haystack - This is a haystack
+    needle = is
+     */
+    fun strStr(haystack: String, needle: String): Int {
+        if (needle.isEmpty()) return 0
+        val longestSubstringPrefix = buildLongestSubstringPrefixArray(needle)
+
+        var j = 0
+        var i = 0
+
+        while (i < haystack.length) {
+            if (haystack[i] != needle[j]) {
+                if (j - 1 < 0) {
+                    j = 0 // because a negative i is not a valid array position
+                    i++
+                } else {
+                    j = longestSubstringPrefix[j - 1] // because longestSubstringPrefix[j - 1] tells us if the pattern contains a prefix that mathes the suffix
+                }
+            } else {
+                j++
+                i++
+            }
+            if (j == needle.length) return i - j
+        }
+        return -1
+    }
+
+    private fun buildLongestSubstringPrefixArray(needle: String): IntArray {
+        val longestSubstringPrefix = IntArray(needle.length)
+        longestSubstringPrefix[0] = 0 //because there is no element before 0th element where prefix matches the suffix
+        var i = 0
+        var j = 1
+
+        while (j < needle.length) {
+            if (needle[i] == needle[j]) {
+                longestSubstringPrefix[j] = i + 1 // this means that array until jth position has a prefix of length i+1 that matches a suffix
+                i++
+                j++
+            } else {
+                if (i - 1 < 0) {
+                    i = 0 // because a negative i is not a valid array position
+                    j++
+                } else {
+                    i = longestSubstringPrefix[i - 1] // not sure why, but that's the algo
+                }
+            }
+        }
+        return longestSubstringPrefix
+    }
+
+    fun naiveApproachFindSubstringInString(haystack: String, needle: String): Int {
+        if (needle.isEmpty()) return 0
+        for (i in haystack.indices) {
+            if (haystack[i] == needle[0]) {
+                var t = i + 1
+                var j = 1
+                while (t < haystack.length && j < needle.length) {
+                    if (haystack[t] == needle[j]) {
+                        t++
+                        j++
+                    } else
+                        break
+                }
+                if (j == needle.length) return i
+            }
+        }
+        return -1
+    }
+}
---
+++ src/test/kotlin/array/ImplementStrStrTest.kt
@@ -0,0 +1,18 @@
+package array
+
+import array.ImplementStrStr
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ImplementStrStrTest {
+    @Test
+    fun `should find a str within a str`() {
+        val o = ImplementStrStr()
+        assertEquals(4, o.strStr("aabaaabaaac", "aabaaac"))
+        assertEquals(4, o.strStr("mississippi", "issip"))
+        assertEquals(8, o.strStr("This is a haystack", "a "))
+        assertEquals(2, o.strStr("This is a haystack", "i"))
+        assertEquals(0, o.strStr("mississippi", ""))
+        assertEquals(-1, o.strStr("mississippi", "missD"))
+    }
+}
